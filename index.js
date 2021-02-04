552f93b76d22fe42c310d95de4855c6b5e844a09
RKRedfern <rkredfern@gmail.com>
2021-02-04 15:44:02 -0500

    commit tree 3d63d59145ab9d57590d9d2d197e5b704e327c7a

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 1;
+const sum2 = 62;
+const multiply = num1 * num2;
+const random = Math.floor( Math.random() * 10 ) + 1;
+const num3 = 4;
+const num4 = 1;
+const mod = num3 % num4;
+const max = Math.max ( 5, 10, 15, 20 );
 
-// Write your code here
